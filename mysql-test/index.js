86409e79821d4e76b0cc57b662c46ef257b4090b
sheng14 <1336840941@qq.com>
2020-08-18 10:19:44 +0800

    node.js操作MySQL

---
+++ mysql-test/index.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql') // 引入数据库插件
+
+const con = mysql.createConnection({ // 创建连接（root默认密码为空）
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: '3306',  // 默认端口号
+    database: 'myBlog' // 连接到myblog数据库（navicat里面的）
+})
+
+con.connect() // 连接数据库
+
+const sql = 'select * from blogs' // 定义查询语句
+con.query(sql, (err, result) => { // 执行查询语句
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(result)
+    }
+})
+con.end() // 关闭连接
