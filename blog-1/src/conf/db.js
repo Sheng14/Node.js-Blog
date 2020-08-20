c2536042770f5bc9fb4b5f139d2c60943231e9c4
sheng14 <1336840941@qq.com>
2020-08-20 13:05:41 +0800

    node.js连接redis-封装工具函数

--- blog-1/src/conf/db.js
+++ blog-1/src/conf/db.js
@@ -1,6 +1,7 @@
 const env = process.env.NODE_ENV
 
 let MYSQL_CONF
+let REDIS_CONF
 
 if (env === 'dev') {
     MYSQL_CONF = {
@@ -9,6 +10,10 @@ if (env === 'dev') {
         password: '',
         port: '3306',
         database: 'myBlog'
+    },
+    REDIS_CONF = {
+        port: 6379,
+        host: '127.0.0.1'
     }
 }
 
@@ -19,9 +24,14 @@ if (env === 'production') {
         password: '',
         port: '3306',
         database: 'myBlog'
+    },
+    REDIS_CONF = {
+        port: 6379,
+        host: '127.0.0.1'
     }
 }
 
 module.exports = {
-    MYSQL_CONF
+    MYSQL_CONF,
+    REDIS_CONF
 }
---
+++ blog-1/src/db/redis.js
@@ -0,0 +1,45 @@
+const redis = require('redis')
+const { REDIS_CONF } = require('../conf/db')
+
+// 创建一个客户端
+const redisClient = redis.createClient(REDIS_CONF.portm, REDIS_CONF.host)
+redisClient.on('error', err => {
+    console.log(err)
+})
+
+// 定义设置redis的方法
+function set (key, val) {
+    if (val === 'object') { // 如果值是对象则转换为json字符串
+        JSON.stringify(val)
+    }
+    redisClient.set(key, val, redis.print)
+}
+
+// 定义获取redis的方法
+function get (key) {
+    const promise = new Promise((resolve, reject) => {
+        redisClient.get(key, (err, val) => { // 获取redis
+            if (err) { // 如果错误存在直接退出
+                reject(err)
+                return
+            }
+            if (val == null) { // 如果找不到key对应的值则返回null
+                resolve(null)
+            }
+            try { // 如果不需要转换为原本的对象则直接返回
+                resolve(
+                    JSON.parse(val)
+                )
+            }
+            catch (ex) {
+                resolve(val)
+            }
+        })
+    })
+    return promise
+}
+
+module.exports = {
+    set,
+    get
+}
