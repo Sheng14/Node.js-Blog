105f24ae894c6c9d1b585ebeb0d8ea51f32a9e75
sheng14 <1336840941@qq.com>
2020-08-21 18:05:09 +0800

    node

---
+++ file-test/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const path = require('path')
+
+const fileName = path.resolve(__dirname, 'data.txt') // 拿到要操作的文件
+
+// 读取文件
+fs.readFile(fileName, (err,data) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log(data.toString()) // 注意拿到的是二进制文件，需要转换成字符串！
+})
+
+// 写入文件
+const content = '帝国长枪兵\n' // 随便写入点东西，这里还换行了！
+const opt = {
+    flag: 'a'
+} // 定义写入的形式，a代表追加写入
+fs.writeFile(fileName, content, opt, (err) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+})
+
+// 判断文件是否存在
+fs.exists(fileName, (state) => {
+    console.log(state)
+})
