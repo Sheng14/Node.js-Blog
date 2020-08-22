a2e3cfc0e836fc25745fcbc3d9b02c6a209731f8
sheng14 <1336840941@qq.com>
2020-08-22 18:16:49 +0800

    密码加密

--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -1,6 +1,8 @@
 const { exec, excape } = require('../db/mysql')
+const { genPassword } = require('../utils/cryp')
 const loginCheck = (username, password) => {
     username = excape(username)
+    password = genPassword(password) // 对密码进行加密（数据库的密码已经加密，这里不加密就对不上，登录失败）
     password = excape(password) // 对传入的参数进行特殊字符的处理，同时下面的也需要去掉‘’
     const sql = `select * from users where username=${username} and password=${password}` // 根据拿到的密码和用户名进行条件查询
     console.log(sql)
