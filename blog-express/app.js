d5a3472f132a6d2d16c3845c251bd578fb7d5740
sheng14 <1336840941@qq.com>
2020-08-23 10:46:40 +0800

    介绍express入口代码（app.js）

--- blog-express/app.js
+++ blog-express/app.js
@@ -1,37 +1,38 @@
-var createError = require('http-errors');
+var createError = require('http-errors'); // 主要是处理路由错误例如404，可以不关心
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var cookieParser = require('cookie-parser'); // 解析cookie方便各地使用
+var logger = require('morgan'); // 处理日志access那一块
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users'); // 引入路由
 
-var app = express();
+var app = express(); // 注册实例，监听客户端请求
 
-// view engine setup
+// view engine setup 这里主要是处理前端引擎的，我们可以不管
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev')); // 注册日志，有一些配置后面讲
+app.use(express.json()); // 处理post请求中的数据 注：content-type是application/json的时候 各地可以通过req.body访问到数据
+app.use(express.urlencoded({ extended: false })); // 处理post请求中的数据 住：content-type不是上面那种情况的时候（如x-www-啥啥啥）
+app.use(cookieParser()); // 注册cookie，各地可以通过req.cookies访问
+app.use(express.static(path.join(__dirname, 'public'))); // 处理静态资源，这里用不到，我们也不用管
 
+// 注册完，接下来就是路由处理
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter); // 注册路由，且加是父路径！
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler 如果路径不符合上面引入的路由就会404，进而触发这个函数
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler 处理程序抛出的问题，如果是线下开发的时候抛出，如果是上线了有bug就返回空对象
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'dev' ? err : {}; // 需与package.json中定义的环境名称一致
 
   // render the error page
   res.status(err.status || 500);
