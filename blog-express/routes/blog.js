adfdd16c1eb2e80072f47a47934e2f9104e647c7
sheng14 <1336840941@qq.com>
2020-08-24 14:27:43 +0800

    开发路由

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -7,15 +7,18 @@ const { getList,
     delBlog
  } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 
 router.get('/list', function(req, res, next) {
     let author = req.query.author || ''
     const keyword = req.query.keyword || ''
     if (req.query.isadmin) {
-        /*const LoginCheckResult = LoginCheck(req)
-        if (LoginCheckResult) {
-            return LoginCheckResult
-        }*/
+        if (req.session.username == null) {
+            res.json(
+                new ErrorModel('未登录')
+            )
+            return
+        }
         author = req.session.username // 也是多写了个s即sessison我他妈
         console.log(req.session.username)
     }
@@ -28,9 +31,52 @@ router.get('/list', function(req, res, next) {
 });
 
 router.get('/detail', function(req, res, next) {
-    res.json({
-        errno: 0,
-        data: [4, 5, 6]
+    const detailResult = getDetail(req.query.id)
+    return detailResult.then((detailData) => {
+        res.json(
+            new SuccessModel(detailData)
+        )
+    })
+})
+
+router.post('/new', loginCheck, (req, res, next) => {
+    req.body.author = req.session.username
+    const result = newBlog(req.body)
+    return result.then((data) => {
+        res.json(
+            new SuccessModel(data)
+        )
+    })
+})
+
+router.post('/update', loginCheck, (req, res, next) => {
+    const result = updateBlog(req.query.id, req.body) // 把要删除的id和postData（即更新的内容）传入
+    return result.then((updateData) => {
+        if (updateData) {
+            res.json(
+                new SuccessModel()
+            )
+        } else {
+            res.json(
+                new ErrorModel('更新博客失败')
+            )
+        }
+    })
+})
+
+router.post('/del', loginCheck, (req, res, next) => {
+    const author = req.session.username
+    const result = delBlog(req.query.id, author)
+    return result.then((deleteData) => {
+        if (deleteData) {
+            res.json(
+                new SuccessModel()
+            )
+        } else {
+           res.json(
+                new ErrorModel('删除博客失败')
+           )
+        }
     })
 })
 
