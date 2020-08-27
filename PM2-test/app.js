71e3007eeb8870f66fae52f5f9c1efd9558609b0
sheng14 <1336840941@qq.com>
2020-08-27 09:39:05 +0800

    PM2常用命令

--- PM2-test/app.js
+++ PM2-test/app.js
@@ -1,11 +1,17 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+    // 模拟日志
+    console.log('no error', Date.now())
+
+    // 模拟错误
+    console.error('errnor', Date.now())
+
     res.setHeader('Content-type', 'application/json')
     res.end(
         JSON.stringify({
             errno: 0,
-            data: 'welcome to PM2!'
+            data: 'welcome to PM2! we are the god!'
         })
     )
 })
